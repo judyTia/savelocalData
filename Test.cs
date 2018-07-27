0d454381d68df2595f3c7e5b36001792f4c2f5c4
judyTia <39672662+judyTia@users.noreply.github.com>
2018-07-27 16:08:01 -0400

    test for local store data
    changes to store local data in unity.
    notice 1 remember to new object event you already new the outside object, the inside object need new too.
               2 EditorWindow store outside function  filed data, even you close unity. It still there.

---
+++ Test.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Test : System.Object
+{
+    public int p = 5;
+    public Color c = Color.white;
+}
---
+++ TestEdit.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+public class TestEdit : EditorWindow
+{
+    public useTest gameData;
+
+    private string gameDataProjectFilePath = "/StreamingAssets/mytest7.json";
+    private bool issecond = true;
+    [MenuItem("Window/TestEdit Editor")]
+    static void Init()
+    {
+        EditorWindow.GetWindow(typeof(TestEdit)).Show();
+        
+    }
+    void OnGUI()
+    {
+        if(!issecond)
+        {
+            SaveAtFirst();
+            issecond = true;
+        }
+
+        if (gameData != null)
+        {
+            
+            SerializedObject serializedObject = new SerializedObject(this);
+            SerializedProperty serializedProperty = serializedObject.FindProperty("gameData");
+            EditorGUILayout.PropertyField(serializedProperty, true);
+
+            serializedObject.ApplyModifiedProperties();
+            if (GUILayout.Button("Save data"))
+            {
+                SaveGameData();
+            }
+        }
+
+
+        if (GUILayout.Button("Load data"))
+        {
+            LoadGameData();
+        }
+    }
+    private void LoadGameData()
+    {
+        string filePath = Application.dataPath + gameDataProjectFilePath;
+
+        if (File.Exists(filePath))
+        {
+            string dataAsJson = File.ReadAllText(filePath);
+            gameData = JsonUtility.FromJson<useTest>(dataAsJson);
+        }
+        else
+        {
+            gameData = new useTest();
+        }
+    }
+
+    private void SaveGameData()
+    {
+
+        string dataAsJson = JsonUtility.ToJson(gameData);
+
+        string filePath = Application.dataPath + gameDataProjectFilePath;
+        File.WriteAllText(filePath, dataAsJson);
+
+    }
+    private void SaveAtFirst()
+    {
+        useTest testData = new useTest();
+        testData.mytest = new Test[4];
+        for (int i = 0; i < 4; i++)
+        {
+            testData.mytest[i] = new Test();
+
+            testData.mytest[i].p = 10;
+            testData.mytest[i].c = Color.yellow;
+        }
+        string dataAsJson = JsonUtility.ToJson(testData);
+
+        string filePath = Application.dataPath + gameDataProjectFilePath;
+        File.WriteAllText(filePath, dataAsJson);
+    }
+}
